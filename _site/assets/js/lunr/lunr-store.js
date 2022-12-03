b33f5312bf627c9768e7eb3d460ba38d4033d0ea
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2022-12-03 22:22:47 +0100

    github pages?

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -292,10 +292,4 @@ var store = [{
         "tags": [],
         "url": "/2015/07/11/love-anything.html",
         "teaser": "/assets/images/love_anything.jpg"
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2022/12/03/welcome-to-jekyll.html",
-        "teaser": "/assets/images/teaser.jpg"
       }]
